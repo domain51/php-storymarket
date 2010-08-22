31bdb36e28d728c4885430d2764ee828b2abc2b7
Travis Swicegood <development@domain51.com>
2010-08-22 13:46:05 -0500

    Initial set of public facing class placeholders

---
+++ src/Storymarket.php
@@ -0,0 +1,26 @@
+<?php
+
+// TODO: handle SPL autoload failure gracefully
+function _storymarket_autoloader($class) {
+    require str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+}
+spl_autoload_register(_storymarket_autoloader);
+
+class Storymarket {
+    public $api_key = null;
+    public $client = null;
+
+    public function __construct($api_key) {
+        $this->api_key = $api_key;
+        $this->client = new Storymarket_Client($this);
+
+        $this->audio = new Storymarket_Content_AudioManager($this);
+        $this->categories = new Storymarket_Content_CategoryManager($this);
+        $this->data = new Storymarket_Content_Data($this);
+        $this->orgs = new Storymarket_Orgs_OrgManager($this);
+        $this->photos = new Storymarket_Content_PhotoManager($this);
+        $this->pricing = new Storymarket_Schemes_PricingSchemeManager($this);
+        $this->rights = new Storymarket_Schemes_RightsSchemeManager($this);
+    }
+}
+
---
+++ src/Storymarket/Schemes/RightsSchemeManager.php
@@ -0,0 +1,5 @@
+<?php
+
+class Storymarket_Schemes_RightsSchemeManager {
+}
+
