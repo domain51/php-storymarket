ffdaca4953e29efb4b1c90ff2600b0404b121d64
Travis Swicegood <development@domain51.com>
2010-10-23 07:33:55 -0500

    first rough pass at CategoryManager

--- src/Storymarket/Categories/CategoryManager.php
+++ src/Storymarket/Categories/CategoryManager.php
@@ -1,5 +1,14 @@
 <?php
 
-class Storymarket_Categories_CategoryManager {
+class Storymarket_Categories_CategoryManager extends Storymarket_Base_Manager {
+    public $resourceClass = 'Storymarket_Categories_Category';
+
+    public function all() {
+        return $this->_handler->doList('/content/category/');
+    }
+
+    public function get($resource) {
+        return $this->_handler->doGet("/content/category/{$resource->id}/");
+    }
 }
 
