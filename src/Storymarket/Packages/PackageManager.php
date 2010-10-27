d0908c4b470e708a86026ac02ee5bd26ce639f10
Travis Swicegood <development@domain51.com>
2010-10-27 08:21:44 -0500

    add in initial pieces for the PackageManager

---
+++ src/Storymarket/Packages/PackageManager.php
@@ -0,0 +1,6 @@
+<?php
+
+class Storymarket_Packages_PackageManager extends Storymarket_Content_ContentManager {
+    public $_url_bit = 'package';
+}
+
---
+++ tests/Storymarket/Packages/PackageManagerTest.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once dirname(dirname(dirname(__FILE__))) . '/bootstrap.php';
+
+class Storymarket_Packages_PackageManagerTest extends StorymarketContentManagerTests {
+}
+
