47252e9608e883c1b4172e224374feec309f78a2
Travis Swicegood <development@domain51.com>
2010-10-27 18:44:38 -0500

    flesh out the flattening of Package *_items fields

--- src/Storymarket/Packages/PackageManager.php
+++ src/Storymarket/Packages/PackageManager.php
@@ -2,5 +2,42 @@
 
 class Storymarket_Packages_PackageManager extends Storymarket_Content_ContentManager {
     public $_url_bit = 'package';
+
+    public function __construct($api, $handler=null, $url_bit=null) {
+        parent::__construct($api, $handler, $url_bit);
+        array_push($this->_flattenFields,
+            'audio_items',
+            'data_items',
+            'photo_items',
+            'text_items',
+            'video_items'
+        );
+    }
+
+    public function toArray($resource) {
+        $array = parent::toArray($resource);
+
+        foreach ($this->_flattenFields as $field) {
+            if (substr($field, -6) !== '_items') {
+                continue;
+            }
+
+            $relatedType = substr($field, 0, -6);
+            $key = "{$relatedType}_items";
+            if (empty($array[$key])) {
+                continue;
+            }
+            $relatedArray = array();
+
+            foreach ($array[$key] as $related) {
+                $related = "/content/{$relatedType}/{$related->id}/";
+                $relatedArray[] = $related;
+            }
+            $array[$key] = $relatedArray;
+        }
+
+        return $array;
+
+    }
 }
 
--- tests/Storymarket/Packages/PackageManagerTest.php
+++ tests/Storymarket/Packages/PackageManagerTest.php
@@ -2,6 +2,51 @@
 
 require_once dirname(dirname(dirname(__FILE__))) . '/bootstrap.php';
 
-class Storymarket_Packages_PackageManagerTest extends StorymarketContentManagerTests {
+class DummyResource {
+    public $id = null;
+}
+
+class Storymarket_Packages_PackageManagerTest
+    extends Storymarket_Content_ContentManagerTest {
+
+    public function assertExpectedItems($type) {
+        $manager = new Storymarket_Packages_PackageManager($this->getMockApi());
+
+        $data = array();
+        for ($i = 0, $l = rand(2, 10); $i < $l; ++$i) {
+            $obj = new DummyResource();
+            $obj->id = rand(100, 200);
+            $data["{$type}_items"][] = array($obj);
+        }
+        $resource = new Storymarket_Packages_Package($manager, $data);
+        $result = $manager->toArray($resource);
+
+        $this->assertEquals(count($data["{$type}_items"]), count($result["{$type}_items"]));
+        foreach ($result["{$type}_items"] as $key => $item) {
+            $expectedId = $data["{$type}_items"][$key]->id;
+            $expected = "/content/{$type}/{$expectedId}/";
+            $this->assertEquals($expected, $item);
+        }
+    }
+
+    public function testAudioItemsAreCompressedWhenConvertingToArray() {
+        $this->assertExpectedItems('audio');
+    }
+
+    public function testDataItemsAreFlattenedAsExpected() {
+        $this->assertExpectedItems('data');
+    }
+
+    public function testPhotoItemsAreFlattenedAsExpected() {
+        $this->assertExpectedItems('photo');
+    }
+
+    public function testTextItemsAreFlattenedAsExpected() {
+        $this->assertExpectedItems('text');
+    }
+
+    public function testVideoItemsAreFlattenedAsExpected() {
+        $this->assertExpectedItems('video');
+    }
 }
 
